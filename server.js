80e1d9688a42e4ecd7102d0c30d964b505007d05
Marina <marvarado19@gmail.com>
2024-10-07 11:35:08 -0700

    finishworkflow

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// TODO: this file :)
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const employees = require('./employees');
+
+app.get('/', (req, res) => {
+  res.send('Hello employees!');
+});
+
+app.get('/employees', (req, res) => {
+  res.json(employees);
+});
+
+app.get('/employees/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  const randomEmployee = employees[randomIndex];
+  res.send(randomEmployee.name);
+});
+
+app.get('/employees/:id', (req, res) => {
+  const employee = employees.find((obj) => obj.id === +req.params.id);
+  if (employee) {
+    res.send(employee.name);
+  } else {
+    res.send('There is not employee with that id.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
